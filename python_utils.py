1e631eaf0ec3b5de4d936515907f44bbb1333b8f
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-25 12:56:54 +0900

    update

--- python_utils.py
+++ python_utils.py
@@ -1,4 +1,5 @@
 import os,sys
+import subprocess
 
 # input: string list (ex. ["aaa","bbb","ccc"])
 # output: string (ex. "aaa,bbb,ccc"
@@ -11,3 +12,15 @@ def list2string(list):
 # https://softhints.com/python-change-directory-parent/
 def getud_name():
   return os.path.abspath(os.curdir).split("/")[-2]
+
+def run_bash(cmd):
+  ret = subprocess.Popen(cmd, shell=True,  stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+  out,err = ret.communicate()
+  return out,err
+
+if __name__ == '__main__':
+  out,err = run_bash("ls")
+  print("*** out ***")
+  print(out)
+  print("*** error ***")
+  print(err)
